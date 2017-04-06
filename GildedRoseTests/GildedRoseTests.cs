44e59b1b6c3b422612a25d3b80f2f161b654b791
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-04-05 19:11:54 -0700

    Complete rework of the solution and its projects - in order to simplify the number of NuGet packages

---
+++ GildedRoseTests/GildedRoseTests.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using GildedRose;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class UpdateQualityTests
+    {
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 10, 20, "+5 Dexterity Vest", 9, 19)]
+        [InlineData("Elixir of the Mongoose", 5, 7, "Elixir of the Mongoose", 4, 6)]
+        public void UpdateQuality_ShouldUpdateTheQualityAndSellin_BySubtractingOneFromEach_GivenNormalItems(string itemName, int sellIn, int quality, string expectedName, int expectedSellIn, int expectedQuality)
+        {
+            //arrange
+            IList<Item> items = new List<Item>()
+            {
+                new Item()
+                {
+                    Name = itemName,
+                    SellIn = sellIn,
+                    Quality = quality
+                }
+            };
+            Program gildedRose = new Program { Items = items };
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            items.Should().HaveCount(1);
+            Item normalItem = items[0];
+            normalItem.Name.Should().Be(expectedName, "because UpdateQuality should not alter the Name of a normal item");
+            normalItem.SellIn.Should().Be(expectedSellIn, "because a normal item should have its SellIn value decreased by one (each day)");
+            normalItem.Quality.Should().Be(expectedQuality, "because a normal item should have its Quality value decreased by one (each day)");
+        }
+
+        [Fact]
+        public void UpdateQuality_ShouldUpdateTheQualityAndSellin_BySubtractingOneFromSellIn_And_AddingOneToQuality_GivenAnAgedItem()
+        {
+            //arrange
+            const string expectedName = "Aged Brie";
+            IList<Item> items = new List<Item>()
+            {
+                new Item()
+                {
+                    Name = expectedName,
+                    SellIn = 2,
+                    Quality = 0
+                }
+            };
+            Program gildedRose = new Program { Items = items };
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            items.Should().HaveCount(1);
+            Item normalItem = items[0];
+            normalItem.Name.Should().Be(expectedName, "because UpdateQuality should not alter the Name of an aged item");
+            normalItem.SellIn.Should().Be(1, "because an aged item should have its SellIn value decreased by one (each day)");
+            normalItem.Quality.Should().Be(1, "because an aged item should have its Quality value increased by one (each day)");
+        }
+
+        [Fact]
+        public void UpdateQuality_ShouldNotUpdateTheQualityAndSellIn_GivenALegendaryItem()
+        {
+            //arrange
+            const string expectedName = "Sulfuras, Hand of Ragnaros";
+            const int expectedSellIn = 0;
+            const int expectedQuality = 80;
+            IList<Item> items = new List<Item>()
+            {
+                new Item()
+                {
+                    Name = expectedName,
+                    SellIn = expectedSellIn,
+                    Quality = expectedQuality
+                }
+            };
+            Program gildedRose = new Program { Items = items };
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            items.Should().HaveCount(1);
+            Item normalItem = items[0];
+            normalItem.Name.Should().Be(expectedName, "because UpdateQuality should not alter the Name of a legendary item");
+            normalItem.SellIn.Should().Be(expectedSellIn, "because a legendary item should NOT have its SellIn value changed ever");
+            normalItem.Quality.Should().Be(expectedQuality, "because an aged item should NOT have its Quality value changed ever");
+        }
+
+        [Fact]
+        public void UpdateQuality_ShouldUpdateTheQualityAndSellIn_ByFOO_And_BAR_GivenABackstagePassItem()
+        {
+            //arrange
+            const string expectedName = "Backstage passes to a TAFKAL80ETC concert";
+            IList<Item> items = new List<Item>()
+            {
+                new Item()
+                {
+                    Name = expectedName,
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            Program gildedRose = new Program { Items = items };
+
+            //act
+            gildedRose.UpdateQuality();
+
+            //assert
+            items.Should().HaveCount(1);
+            Item normalItem = items[0];
+            normalItem.Name.Should().Be(expectedName, "because UpdateQuality should not alter the Name of a legendary item");
+            normalItem.SellIn.Should().Be(14, "because a Backstage Pass item should have its SellIn value decreased by one (each day)");
+            normalItem.Quality.Should().Be(21, "because a Backstage Pass item should have its Quality value increased by one (each day)");
+        }
+    }
+}
--- GildedRoseTests/Properties/AssemblyInfo.cs
+++ GildedRoseTests/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GildedRose.Console")]
+[assembly: AssemblyTitle("GildedRoseTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DSHS")]
-[assembly: AssemblyProduct("GildedRose.Console")]
-[assembly: AssemblyCopyright("Copyright © DSHS 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GildedRoseTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("99f174df-0709-4d59-bd90-f1d9f64fd023")]
+[assembly: Guid("46b8a2ed-bc60-47d7-8153-e2c6bf2ea73d")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
